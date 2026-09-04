4b8a147ae69495da0ba27b0c2fac4de48cdb71c1
WanakitsamphanTh <s2101125@sendai-nct.jp>
2026-09-04 22:50:34 +0700

    test DTB

--- kernel/kmain.cpp
+++ kernel/kmain.cpp
@@ -1,4 +1,5 @@
 #include <climits>
+#include <cstdint>
 #include <kernel_core.hpp>
 #include <kernel/io.hpp>
 #include "mstd/monadic/maybe.hpp"
@@ -13,20 +14,21 @@ using mstd::scope_guard;
 
 char str[] = {'O','S',' ', 'S', 'e', 'n', 'd', 'a','i'};
 
-extern "C" int kmain(uint64_t dtb){
+extern "C" int kmain(uintptr_t dtb){
     uart_puts("Try write/writeln\n");
     MK::KernelConsole console;
-    console.writeln("[DTB] ", dtb);
+
+    console.writef("[DTB] addr {} ", dtb);
     console.writef("[kernel] start {8h} end {8h} size {}\n", _kernel_start, _kernel_end, _kernel_size);
     console.writef("[rodata] start {8h} end {8h} size {}\n", _rodata_start, _rodata_end, _rodata_size);
     int x = 100;
 
-    /*console.write("Hello world #1\n");
+    console.write("Hello world #1\n");
     console.writeln("Hello world #2");
     console.writeln("integer ", 0);
     console.writeln("x =  ", x);
     console.writeln("&x = ", &x);
-    console.writeln("Hello",'!', " it's me", '\n');*/
+    console.writeln("Hello",'!', " it's me", '\n');
 
     console.writef("Hello {06} {+06} {+06} {} {06} {06} {}\n", 
                     x, x, -x, &x, 10, -1, 'c');
